59f2d523f60e157211f6ccd39d81b9e946b25020
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2021-12-09 14:17:22 -0600

    Creating function to render map and initial marker

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 const APIKey = 'pk.eyJ1Ijoibmlja28xIiwiYSI6ImNrd3o2MTU5ejBydGwybm1raXNrdzVyMTYifQ.GJ6eL_Q7ltDLLzW572Q5iA';
+
+let lat;
+let long;
+
+if (!navigator.geolocation) {
+  console.log('error');
+} else {
+  navigator.geolocation.getCurrentPosition(location => {
+    lat = location.coords.latitude;
+    long = location.coords.longitude;
+    mapboxgl.accessToken = `${APIKey}`;
+    
+    const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [long, lat],
+    zoom: 12
+    });
+
+    const startingMarker = new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+  });
+}
